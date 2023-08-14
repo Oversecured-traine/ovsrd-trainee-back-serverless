b3dc9e1ed30110048fb90a30dd7939cf7630941c
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-08-14 04:12:11 +0300

    added scripts for local usage

---
+++ src/local-usage scripts/columns-table.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+AWS.config.update({
+    region: 'local', 
+    endpoint: 'http://localhost:8000',
+});
+
+const dynamodb = new AWS.DynamoDB();
+
+const params = {
+    TableName: 'adavydova-columns',
+    KeySchema: [
+        { AttributeName: 'columnID', KeyType: 'HASH' },
+    ],
+    AttributeDefinitions: [
+        { AttributeName: 'columnID', AttributeType: 'S' },
+        { AttributeName: 'columnIndex', AttributeType: 'N' },
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1,
+    },
+};
+
+dynamodb.createTable(params, (err, data) => {
+    if (err) console.error("Ошибка при создании таблицы:", err);
+    else console.log("Таблица успешно создана:", data);
+});
